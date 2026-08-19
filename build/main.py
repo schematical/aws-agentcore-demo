56247955286abd8e75ef3e5840752011fa1aca6b
Matt Lea <matt@schematical.com>
2026-08-19 16:20:25 -0500

    Added the browser.

--- build/main.py
+++ build/main.py
@@ -75,7 +75,7 @@ async def initialize_browser_session():
         await browser_session.start()
 
         bedrock_chat = ChatBedrockConverse(
-            model_id="us.anthropic.claude-3-7-sonnet-20250219-v1:0", # us.amazon.nova-2-lite-v1:0
+            model_id="us.amazon.nova-2-lite-v1:0",#"us.anthropic.claude-3-7-sonnet-20250219-v1:0", # us.amazon.nova-2-lite-v1:0
             region_name=AWS_REGION,
         )
 
